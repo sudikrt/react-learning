a77fb48e04c3302156146b8373664f1831017bd6
madhura <madhuraadiga74@gmail.com>
2024-01-05 23:57:58 +0400

    ep3-mid-jsx

--- app.js
+++ app.js
@@ -21,7 +21,14 @@ const rootDiv = React.createElement (
     parent
 )
 console.log (heading);
-const jsxHeading = <h1 id="heading">JSX heading</h1>;
+const jsxHeading = <h1 id="heading" className="head">JSX heading</h1>;
+
+//to wirte in multiple line need to wrap in ()
+// beacuse babel needs to understand it
+const jsxHeading1 = (<h1 id="heading2" 
+className="head">JSX heading1</h1>);
+
+
 console.log (jsxHeading);
 const root =  ReactDOM.createRoot (document.getElementById ("root"));
 root.render (
