48ced3df5ed5a58b29947a6e8915d79e7b19e11f
madhura <madhuraadiga74@gmail.com>
2024-01-10 23:09:20 +0400

    ep-4.1 resto-design

--- app.js
+++ app.js
@@ -1,45 +1,96 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement (
-    "h1", {
-        id : "heading",
-        xyz : "abc"
-    }, "Hello from react"
-);
-const parent = React.createElement (
-    "div",
-    {id : "parent",},
-    [heading, React.createElement (
-        "h1", {
-        }, "Hello heading 2"
-    )]
-)
-const rootDiv = React.createElement (
-    "div",
-    {id : "rootDiv"},
-    parent
-)
-console.log (heading);
-const jsxHeading = <h1 id="heading" className="head">JSX heading</h1>;
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://www.designmantic.com/logo-images/167171.png?company=Company%20Name&slogan=&verify=1">
+                </img>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
 
-//to wirte in multiple line need to wrap in ()
-// beacuse babel needs to understand it
-const JsxHeading1 = () =>  (<h1 id="heading2" 
-className="head">JSX heading1</h1>);
-
-const jsxElement =  (<h1 id="heading2" 
-className="head">JSX Element</h1>);
-
-const FunctionalComp = () => (<div>
-    <JsxHeading1></JsxHeading1>
-    {jsxElement}
-    <h1>Demo React functional Comp</h1>
-</div>);
+const RestroCard = () => {
+    return (
+        <div className="restro-card"> 
+            <h1>Rest -1</h1>
+        </div>
+    )
+}
 
 
-console.log (jsxHeading);
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="restro-container">
+                <RestroCard></RestroCard>
+            </div>
+        </div>
+    )
+}
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header></Header>
+            <Body></Body>
+        </div>
+    )
+}
+
 const root =  ReactDOM.createRoot (document.getElementById ("root"));
 root.render (
-    <FunctionalComp></FunctionalComp>
+    <AppLayout></AppLayout>
 );
+
+// const heading = React.createElement (
+//     "h1", {
+//         id : "heading",
+//         xyz : "abc"
+//     }, "Hello from react"
+// );
+// const parent = React.createElement (
+//     "div",
+//     {id : "parent",},
+//     [heading, React.createElement (
+//         "h1", {
+//         }, "Hello heading 2"
+//     )]
+// )
+// const rootDiv = React.createElement (
+//     "div",
+//     {id : "rootDiv"},
+//     parent
+// )
+// console.log (heading);
+// const jsxHeading = <h1 id="heading" className="head">JSX heading</h1>;
+
+// //to wirte in multiple line need to wrap in ()
+// // beacuse babel needs to understand it
+// const JsxHeading1 = () =>  (<h1 id="heading2" 
+// className="head">JSX heading1</h1>);
+
+// const jsxElement =  (<h1 id="heading2" 
+// className="head">JSX Element</h1>);
+
+// const FunctionalComp = () => (<div>
+//     <JsxHeading1></JsxHeading1>
+//     {jsxElement}
+//     <h1>Demo React functional Comp</h1>
+// </div>);
+// console.log (jsxHeading);
+
+// const root =  ReactDOM.createRoot (document.getElementById ("root"));
+// root.render (
+//     <FunctionalComp></FunctionalComp>
+// );
