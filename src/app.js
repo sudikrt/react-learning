c356ad2dc78d56f7983316ff8fe923ab5c8f65b7
sudarshan <sudarshankaranth18@gmail.com>
2024-01-24 20:51:18 +0400

    Added path

--- src/app.js
+++ src/app.js
@@ -3,16 +3,32 @@ import ReactDOM from "react-dom/client";
 
 import Header from "./components/header";
 import Body from "./components/body";
+import Error from "./components/error";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import About from "./components/about";
 import ContactUs from "./components/contactus";
+import RestroMenuPage from "./components/restroMenuPage";
+
 
 const AppLayout = () => {
     return (
         <div className="app">
             <Header></Header>
-            <Body></Body>
+            {
+                /**
+                 * if my path is / then it should have body <Body></Body>
+                 * if my path is /about then its about component <About></About>
+                 * if my path is /contactus then its contact us component <ContactUs/>
+                 * 
+                 * <Outlet> is replaced by the component based on path
+                 * 
+                 * Single page application
+                 * 
+                 * if you want to navigate don't use anchor tag
+                 */
+            }
+            <Outlet></Outlet>
         </div>
     )
 }
@@ -21,16 +37,27 @@ const appRouter = createBrowserRouter (
     [
         {
             path : "/",
-            element : <AppLayout></AppLayout>
+            element : <AppLayout></AppLayout>,
+            errorElement : <Error></Error>,
+            children : [
+                {
+                    path : "/",
+                    element : <Body></Body>
+                },
+                {
+                    path : "/aboutus",
+                    element : <About></About>
+                },
+                {
+                    path : "/contactus",
+                    element : <ContactUs/>
+                },
+                {
+                    path : "/restro/:resId", /**resId is dynamic */
+                    element : <RestroMenuPage></RestroMenuPage>
+                }
+            ]
         },
-        {
-            path : "/about",
-            element : <About></About>
-        },
-        {
-            path : "/contactus",
-            element : <ContactUs/>
-        }
     ]
 );
 
