44df0a4e0d35e4f1e4f988590f6678341dd42af8
sudikrt <sudarshankaranth18@gmail.com>
2024-02-01 22:35:32 +0400

    Ep-08-03

--- src/components/about.js
+++ src/components/about.js
@@ -1,12 +1,59 @@
+import React from "react";
 import UserCard from "./usercard";
 import UserClass from "./userClass";
-const About = () => {
-    return (
-        <div>
-            <h1>About us</h1>
-            <UserCard name={"Sud-functional"} location="Blore"></UserCard>
-            <UserClass name={"Sud-class"} location="Mlore"></UserClass>
-        </div>
-    );
+class About extends React.Component {
+    constructor (props) {
+        super (props);
+        console.log ('About constructor is called : ');
+    }
+    render () {
+        console.log ('About render is called : ');
+        return (
+            <div>
+                <h1>About us</h1>
+                <UserClass name={"Sud-class"} location="Mlore"></UserClass>
+                {
+                    /**
+                     * for example first About comp's is invoked,
+                     * first thing when the instance of the class loads, then constructor is invoked
+                     * then render is called 
+                     */
+                }
+            </div> 
+        )
+    }
+
+    /**
+     * Order in which life cycle methods are called.
+     *  1. constructor
+     *  2. render
+     *  3. componentDidMount
+     * notifies the component when the comp is loader, called after the current comp loading completes
+     * called when the comp is mounted on to the web page
+     * 
+     * once all the children componentDidMount () called, then the parent componentDidMount is called
+     * 
+     * why do we need componentDidMount is needed  ? -  used to make api calls after the component is loaded into the dom,
+     * 
+     */
+    componentDidMount () {
+        console.log ('About componentDidMount is called : ');
+    }
 }
+// const About = () => {
+//     return (
+//         <div>
+//             <h1>About us</h1>
+//             <UserCard name={"Sud-functional"} location="Blore"></UserCard>
+//             <UserClass name={"Sud-class"} location="Mlore"></UserClass>
+//             {
+//                 /**
+//                  * for example first About comp's is invoked,
+//                  * first thing when the instance of the class loads, then constructor is invoked
+//                  * then render is called 
+//                  */
+//             }
+//         </div>
+//     );
+// }
 export default About;
--- src/components/userClass.js
+++ src/components/userClass.js
@@ -19,8 +19,13 @@ class UserClass extends React.Component {
             count : 0,
             count1 : 1
         };
+        console.log ('UserClass constructor is called : ');
+    }
+    componentDidMount () {
+        console.log ('UserClass componentDidMount is called : ');
     }
     render () {
+        console.log ('UserClass render is called : ');
         //this can be destructured
         const {name, location} = this.props;
         return (
@@ -30,6 +35,15 @@ class UserClass extends React.Component {
                 <h2>Name : {this.props.name}</h2>
                 <h2>Location : {location}</h2>
                 <h2>Contact : @sud</h2>
+                <button onClick={(e) => {
+                    /**
+                     * we never modify the state variables directly
+                     * like this.state.count ++
+                     * you can update both state variable togather
+                     * state variable is a big object, it will update only the passed valiables - values like it will update the portion of it. not all the other variables
+                     */
+                    this.setState ({count : ++this.state.count});
+                }}>Increment</button>
             </div>
         );
     }
